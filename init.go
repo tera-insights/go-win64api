34c1f6efe49bb20aa7cb93cc882c4feed0076d8a
Julio J. Cabrera <jjcabreraterainsights@gmail.com>
2021-12-25 16:39:35 -0500

    loading userenv.dll

--- init.go
+++ init.go
@@ -1,11 +1,31 @@
 package winapi
 
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+var required_loadable_dlls []string = []string{
+	"userenv.dll",
+}
+
 func init() {
-	modAdvapi32.Load()
-	modKernel32.Load()
-	modNetapi32.Load()
-	modOffreg.Load()
-	modSecur32.Load()
-	modUserenv.Load()
-	modWtsapi32.Load()
+	// cmd := exec.Command("cmd", "/C", "for /f %s in ('dir /b *.dll') do regsvr32 /s %s")
+	// cmd.Dir = "C:\\Windows\\system32"
+	// if out, err := cmd.CombinedOutput(); err != nil {
+	// 	log.Printf("[Warning] failed to reload DLL %v with error: %s and output: %s", nil, err.Error(), out)
+	// } else {
+	// 	log.Printf("[Info] successfully reloaded system DLL %v", nil)
+	// }
+
+	for _, dll := range required_loadable_dlls {
+		cmd := exec.Command("cmd", "/C", fmt.Sprintf("regsvr32 /s %s", dll))
+		cmd.Dir = "C:\\Windows\\System32"
+		if out, err := cmd.CombinedOutput(); err != nil {
+			log.Printf("[Warning] failed to register DLL %s with error: %s and output: %s", dll, err.Error(), out)
+		} else {
+			log.Printf("[Info] successfully registered system DLL %s", dll)
+		}
+	}
 }
