7cf2f59fdc95e60b0d194bacf7e86454486da12d
Julio J. Cabrera <jjcabreraterainsights@gmail.com>
2021-12-25 16:46:44 -0500

    cleaned up

--- init.go
+++ init.go
@@ -11,14 +11,6 @@ var required_loadable_dlls []string = []string{
 }
 
 func init() {
-	// cmd := exec.Command("cmd", "/C", "for /f %s in ('dir /b *.dll') do regsvr32 /s %s")
-	// cmd.Dir = "C:\\Windows\\system32"
-	// if out, err := cmd.CombinedOutput(); err != nil {
-	// 	log.Printf("[Warning] failed to reload DLL %v with error: %s and output: %s", nil, err.Error(), out)
-	// } else {
-	// 	log.Printf("[Info] successfully reloaded system DLL %v", nil)
-	// }
-
 	for _, dll := range required_loadable_dlls {
 		cmd := exec.Command("cmd", "/C", fmt.Sprintf("regsvr32 /s %s", dll))
 		cmd.Dir = "C:\\Windows\\System32"
